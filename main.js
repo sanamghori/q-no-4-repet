8e26a48362bb0664eb22c3e9fa95528c159f6b7c
sanamghori <161497237+sanamghori@users.noreply.github.com>
2024-08-06 15:35:44 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var nam = "anaya khan";
+console.log("tolowercase", nam.toLocaleLowerCase());
+console.log("touppercase", nam.toLocaleUpperCase());
+//tital case k liy function banan hoga
+function titalCase(choice) {
+    if (choice === void 0) { choice = " totitalcase Anaya Khan"; }
+    console.log(choice);
+}
+titalCase();
